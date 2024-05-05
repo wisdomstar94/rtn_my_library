d7a02469174d47a979cfb3844852810ee656f6a8
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-05 21:59:59 +0900

    v0.0.8 : fix

--- dispose.sh
+++ dispose.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-
-FILE = "./android/build.gradle"
-
-if [ ! -e $FILE ]; then
-	mv ./android/build.gradle.rtn ./android/build.gradle
+if [ -d node_modules/rtn_my_library ]; then
+  pushd node_modules/rtn_my_library
+  FILE = "./android/build.gradle"
+  if [ ! -e $FILE ]; then
+    mv ./android/build.gradle.rtn ./android/build.gradle
+  fi
+  popd
 fi
