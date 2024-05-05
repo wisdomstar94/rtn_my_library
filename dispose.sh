f67ef5a466d1680d07dfb3aa1daa58549934f944
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-05 22:04:37 +0900

    0.0.9 : dispose.js 작성.

--- dispose.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-if [ -d node_modules/rtn_my_library ]; then
-  pushd node_modules/rtn_my_library
-  FILE = "./android/build.gradle"
-  if [ ! -e $FILE ]; then
-    mv ./android/build.gradle.rtn ./android/build.gradle
-  fi
-  popd
-fi
