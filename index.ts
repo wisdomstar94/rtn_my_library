2c4a4e3a8acdffa5efe0c29a9d716d1f17ed8467
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-06 14:11:56 +0900

    v0.0.19 : test

---
+++ index.ts
@@ -0,0 +1,5 @@
+import _gallery from './js/NativeRtnMyGallery';
+import _library from './js/NativeRtnMyLibrary';
+
+export const gallery = _gallery;
+export const library = _library;
--- js/index.ts
+++
@@ -1,5 +0,0 @@
-import _gallery from './NativeRtnMyGallery';
-import _library from './NativeRtnMyLibrary';
-
-export const gallery = _gallery;
-export const library = _library;
