ec0d9b7e655b870f64f555119512caa062724f55
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-07 16:10:02 +0900

    v0.0.32 : test

--- ios/RTNMyLibrary.mm
+++ ios/RTNMyLibrary.mm
@@ -23,26 +23,47 @@ RCT_EXPORT_MODULE()
 }
 
 -(void)requestGalleryImage:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
-    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (status == PHAuthorizationStatusAuthorized) {
-                // 권한이 허용되었을 때만 갤러리를 엽니다.
-                self.imagePickerController = [[ImagePickerControllerViewController alloc] init];
-                __weak __typeof__(self) weakSelf = self;
-                self.imagePickerController.imageSelectionCallback = ^(NSDictionary *imageInfo) {
-                  NSLog(@"Selected Image Info: %@", imageInfo);
-                  resolve(@"success");
-                  // [weakSelf handleImageSelection:imageInfo];
-                };
-                [self.imagePickerController chooseImage];
-            } else {
-                // 권한이 거부되었거나 제한되었을 때의 처리
-                NSLog(@"갤러리 접근 권한이 거부되었습니다.");
-                resolve(@"permission-deny");
-                // 사용자에게 권한 설정을 변경하도록 알릴 수 있습니다.
+    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    switch (status) {
+        case PHAuthorizationStatusAuthorized:
+            // 이미 권한이 허용되어 있음
+            resolve(@"이미 권한이 허용되어 있음");
+            break;
+        case PHAuthorizationStatusNotDetermined:
+            // 권한을 요청함
+            {
+                  resolve(@"권한을 요청함");
+//                [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+//                    dispatch_async(dispatch_get_main_queue(), ^{
+//                        if (status == PHAuthorizationStatusAuthorized) {
+//                            // 권한이 허용되었을 때만 갤러리를 엽니다.
+//                            self.imagePickerController = [[ImagePickerControllerViewController alloc] init];
+//                            __weak __typeof__(self) weakSelf = self;
+//                            self.imagePickerController.imageSelectionCallback = ^(NSDictionary *imageInfo) {
+//                              NSLog(@"Selected Image Info: %@", imageInfo);
+//                              resolve(@"success");
+//                              // [weakSelf handleImageSelection:imageInfo];
+//                            };
+//                            [self.imagePickerController chooseImage];
+//                        } else {
+//                            // 권한이 거부되었거나 제한되었을 때의 처리
+//                            NSLog(@"갤러리 접근 권한이 거부되었습니다.");
+//                            resolve(@"permission-deny");
+//                            // 사용자에게 권한 설정을 변경하도록 알릴 수 있습니다.
+//                        }
+//                    });
+//                }];
             }
-        });
-    }];
+            break;
+        case PHAuthorizationStatusDenied:
+        case PHAuthorizationStatusRestricted:
+            // 권한이 거부되었거나 제한되었음
+            NSLog(@"갤러리 권한이 거부되었습니다.");
+            resolve(@"권한이 거부되었거나 제한되었음");
+            break;
+        default:
+            break;
+    }
   
 //  [self selectImage];
 //  [ImagePickerManager.store getGallery:^(UIImage * image) {
