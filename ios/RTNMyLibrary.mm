21b4fdc966799b757d56670aad5c973bd581eea6
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-07 14:30:57 +0900

    v0.0.28 : test

--- ios/RTNMyLibrary.mm
+++ ios/RTNMyLibrary.mm
@@ -1,6 +1,7 @@
 #import "RTNMyLibrary.h"
 #import <sys/utsname.h>
 #import "ImagePickerControllerViewController.h"
+#import <Photos/Photos.h>
 
 @interface RTNMyLibrary ()
 
@@ -21,14 +22,26 @@ RCT_EXPORT_MODULE()
 }
 
 -(void)requestGalleryImage:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
-  self.imagePickerController = [[ImagePickerControllerViewController alloc] init];
-  __weak __typeof__(self) weakSelf = self;
-  self.imagePickerController.imageSelectionCallback = ^(NSDictionary *imageInfo) {
-    NSLog(@"Selected Image Info: %@", imageInfo);
-    resolve(@"success");
-//    [weakSelf handleImageSelection:imageInfo];
-  };
-  [self.imagePickerController chooseImage];
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (status == PHAuthorizationStatusAuthorized) {
+                // 권한이 허용되었을 때만 갤러리를 엽니다.
+                self.imagePickerController = [[ImagePickerControllerViewController alloc] init];
+                __weak __typeof__(self) weakSelf = self;
+                self.imagePickerController.imageSelectionCallback = ^(NSDictionary *imageInfo) {
+                  NSLog(@"Selected Image Info: %@", imageInfo);
+                  resolve(@"success");
+                  // [weakSelf handleImageSelection:imageInfo];
+                };
+                [self.imagePickerController chooseImage];
+            } else {
+                // 권한이 거부되었거나 제한되었을 때의 처리
+                NSLog(@"갤러리 접근 권한이 거부되었습니다.");
+                resolve(@"permission-deny");
+                // 사용자에게 권한 설정을 변경하도록 알릴 수 있습니다.
+            }
+        });
+    }];
   
 //  [self selectImage];
 //  [ImagePickerManager.store getGallery:^(UIImage * image) {
